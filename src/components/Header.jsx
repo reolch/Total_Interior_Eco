c9cc7874160d489d69193cbd854e8120c115a5b6
Ambition <AmbitionJP@github.com>
2024-06-26 20:10:49 +0900

    CSSのリファクタリング #2 HeaderコンポーネントをBEMに準拠するように修正した

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,35 +10,35 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="container">
-        <h1 className='playfair-display'>Total Interior Eco</h1>
-        <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
-          <ul>
-            <li>
-              <a href="/">Home</a>
+      <div className="header__container">
+        <h1 className="header__title playfair-display-900">Total Interior Eco</h1>
+        <nav className={`navbar ${isMenuOpen ? 'navbar--open' : ''}`}>
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <a className="navbar__link" href="/">Home</a>
             </li>
-            <li>
-              <a href="/about">About</a>
+            <li className="navbar__item">
+              <a className="navbar__link" href="/about">About</a>
             </li>
-            <li>
-              <a href="/gallery">Gallery</a>
+            <li className="navbar__item">
+              <a className="navbar__link" href="/gallery">Gallery</a>
             </li>
-            {/* <li>
-              <a href="/services">Services</a>
+            {/* <li className="navbar__item">
+              <a className="navbar__link" href="/services">Services</a>
             </li> */}
-            <li>
-              <a href="/contact">Contact</a>
+            <li className="navbar__item">
+              <a className="navbar__link" href="/contact">Contact</a>
             </li>
           </ul>
         </nav>
-        <div className="hamburger" onClick={toggleMenu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+        <div className="header__hamburger" onClick={toggleMenu}>
+          <span className="header__bar"></span>
+          <span className="header__bar"></span>
+          <span className="header__bar"></span>
         </div>
-        <div className="ribbon-drop-wrapper">
-          <div className="ribbon-content-wrapper">
-            <a href="">Innovative Designs, Timeless Comfort.</a>
+        <div className="ribbon">
+          <div className="ribbon__content">
+            <a className="ribbon__link" href="">Innovative Designs, Timeless Comfort.</a>
           </div>
         </div>
       </div>
